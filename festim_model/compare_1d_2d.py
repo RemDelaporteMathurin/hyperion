f943a576d6165314f7f3a8a6af04274e8c3701f4
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-02-15 16:47:02 -0500

    compare 1/10 height-to-diameter

--- festim_model/compare_1d_2d.py
+++ festim_model/compare_1d_2d.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
+import pint
+
+from run_comparison import T_values
+
+ureg = pint.UnitRegistry()
+ureg.setup_matplotlib()
+
+plt.gca().xaxis.set_units(ureg.hour)
 
 cmap = plt.get_cmap("Reds")
 
-T_values = np.linspace(730, 900, num=4)
 norm = Normalize(vmin=700, vmax=900)
 
 for T in T_values:
@@ -15,10 +22,10 @@ for T in T_values:
         f"results/{T:.0f}K/2d/derived_quantities.csv", delimiter=",", names=True
     )
 
-    t_1d = data_1d["ts"]
-    t_2d = data_2d["ts"]
-    flux_1d = np.abs(data_1d["Flux_surface_3_solute"])
-    flux_2d = np.abs(data_2d["Flux_surface_3_solute"])
+    t_1d = data_1d["ts"] * ureg.s
+    t_2d = data_2d["ts"] * ureg.s
+    flux_1d = np.abs(data_1d["Flux_surface_3_solute"]) * ureg.particle * ureg.s**-1
+    flux_2d = np.abs(data_2d["Flux_surface_3_solute"]) * ureg.particle * ureg.s**-1
     plt.plot(t_1d, flux_1d, color=cmap(norm(T)))
     plt.plot(t_2d, flux_2d, color=cmap(norm(T)))
 
@@ -33,5 +40,12 @@ for T in T_values:
         f"  {T:.0f} K", (t_1d[-1], (flux_1d[-1] + flux_2d[-1]) / 2), color=cmap(norm(T))
     )
 
+plt.xlabel(f"Time ({plt.gca().xaxis.get_units()})")
+plt.ylabel(f"Permeation flux ({plt.gca().yaxis.get_units():~P})")
+
 plt.xlim(left=0)
+plt.ylim(bottom=0)
+
+plt.gca().spines[["right", "top"]].set_visible(False)
+
 plt.show()
--- festim_model/run_comparison.py
+++ festim_model/run_comparison.py
@@ -1,36 +1,38 @@
 from festim_model import make_model
 import numpy as np
 
-salt_thickness = 8e-3
-salt_diameter = 70e-3
+salt_thickness = 10e-3
+salt_diameter = 100e-3
 
 T_values = np.linspace(730, 900, num=4)
 
-for T in T_values:
-    model_2d = make_model(
-        salt_thickness,
-        salt_diameter,
-        nx=40,
-        ny=20,
-        folder=f"results/{T:.0f}K/2d",
-        two_dimensional=True,
-    )
+if __name__ == "__main__":
 
-    model_2d.T.value = T
+    for T in T_values:
+        model_2d = make_model(
+            salt_thickness,
+            salt_diameter,
+            nx=40,
+            ny=20,
+            folder=f"results/{T:.0f}K/2d",
+            two_dimensional=True,
+        )
 
-    model_2d.initialise()
-    model_2d.run()
+        model_2d.T.value = T
 
-    model_1d = make_model(
-        salt_thickness,
-        salt_diameter,
-        nx=40,
-        ny=20,
-        folder=f"results/{T:.0f}K/1d",
-        two_dimensional=False,
-    )
+        model_2d.initialise()
+        model_2d.run()
 
-    model_1d.T.value = T
+        model_1d = make_model(
+            salt_thickness,
+            salt_diameter,
+            nx=40,
+            ny=20,
+            folder=f"results/{T:.0f}K/1d",
+            two_dimensional=False,
+        )
 
-    model_1d.initialise()
-    model_1d.run()
+        model_1d.T.value = T
+
+        model_1d.initialise()
+        model_1d.run()
