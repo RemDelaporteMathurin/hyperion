34d206ca6126eda44629cef7d5c72fcf0580f1f0
jaronfcota <67432181+jaronfcota@users.noreply.github.com>
2024-08-05 13:20:41 -0400

    modified run_comp and compare to do 2d models

--- festim_model/festim_model_copy.py
+++ festim_model/festim_model_copy.py
@@ -1,13 +1,15 @@
 import festim as F
 import fenics as f
 import h_transport_materials as htm
+import numpy as np
 
 #from cylindrical_integrator import CylindricalFlux
 
 print(f"HTM version {htm.__version__}")
 
+P_up = 1000
 
-def make_model(thickness, diameter, nx, ny, two_dimensional: bool, folder="results"):
+def make_model(thickness, diameter, nx, ny, two_dimensional: bool, folder="results", pressure = P_up):
     fenics_mesh = f.RectangleMesh(
         f.Point(0, 0), f.Point(diameter / 2, thickness), nx, ny
     )
@@ -45,16 +47,24 @@ def make_model(thickness, diameter, nx, ny, two_dimensional: bool, folder="resul
     )
     model.mesh = my_mesh
 
-    flibe_diffusivity = htm.diffusivities.filter(material=htm.FLIBE).mean()
+    # Using calderoni's numbers instead of the mean
+    flibe_diffusivity = htm.diffusivities.filter(material=htm.FLIBE, author = 'calderoni')[0]
+    # Adding flibe solubility properties
+    flibe_solubility = htm.solubilities.filter(material=htm.FLIBE, author = 'calderoni')[0]
     flibe = F.Material(
         id=1,
         D_0=flibe_diffusivity.pre_exp.magnitude,
         E_D=flibe_diffusivity.act_energy.magnitude,
+        S_0 = flibe_solubility.pre_exp.magnitude,
+        E_S = flibe_solubility.act_energy.magnitude
     )
+
+
 
     model.materials = flibe
 
-    upstream_pressure = F.HenrysBC(surfaces=[1], H_0=1e17, E_H=0, pressure=1)
+    # Using the actual flibe properties and pressure (~100s of Pa)
+    upstream_pressure = F.HenrysBC(surfaces=[1], H_0=flibe.S_0, E_H=flibe.E_S, pressure=P_up)
     recombination_lateral = F.DirichletBC(surfaces=[2], value=0, field="solute")
     recombination_top = F.DirichletBC(surfaces=[3], value=0, field="solute")
     model.boundary_conditions = [
@@ -68,12 +78,13 @@ def make_model(thickness, diameter, nx, ny, two_dimensional: bool, folder="resul
     model.T = F.Temperature(760)
 
     model.settings = F.Settings(
-        absolute_tolerance=1e4, relative_tolerance=1e-10, final_time=7 * 3600
+        absolute_tolerance=1e4, relative_tolerance=1e-10, final_time=8 * 3600
     )
     model.dt = F.Stepsize(initial_value=10, stepsize_change_ratio=1.1)
 
+    # Changing both of the derived quantities to be cylindrical surface flux
     derived_quantities = F.DerivedQuantities(
-        [F.HydrogenFlux(surface=2), F.SurfaceFluxCylindrical(field = "solute", surface=3)],
+        [F.SurfaceFluxCylindrical(field = "solute", surface=2), F.SurfaceFluxCylindrical(field = "solute", surface=3)],
         filename=f"{folder}/derived_quantities.csv",
     )
 
@@ -85,6 +96,27 @@ def make_model(thickness, diameter, nx, ny, two_dimensional: bool, folder="resul
     # model.log_level = 20
 
     return model
+
+
+
+
+# Function for computing analytical solution to flux
+def downstream_flux_salt(t, P_up, L, permeability, D):
+    """calculates the downstream H flux at a given time t
+
+    Args:
+        t (float, np.array): the time
+        P_up (float): upstream partial pressure of H
+        permeability (float): salt permeability
+        L (float): salt thickness
+        D (float): diffusivity of H in the salt
+
+    Returns:
+        float, np.array: the downstream flux of H
+    """
+    n_array = np.arange(1, 10000)[:, np.newaxis]
+    summation = np.sum((-1)**n_array * np.exp(-(np.pi * n_array)**2 * D/L**2 * t), axis=0)
+    return P_up * permeability / L * (2*summation + 1)
 
 
 if __name__ == "__main__":
